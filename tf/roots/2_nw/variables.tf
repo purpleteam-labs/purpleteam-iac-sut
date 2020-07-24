578d4989b06e6acd324495caf2c4a21d78eef2c9
Kim Carter <Kim.Carter@binarymist.net>
2020-07-25 09:06:15 +1200

    Tweaked variables in variables.tf

--- tf/roots/2_nw/variables.tf
+++ tf/roots/2_nw/variables.tf
@@ -1,14 +1,29 @@
-variable "aws_region" {
+variable "AWS_REGION" {
   description = "AWS region in use."
   type = string
 }
-variable "aws_profile" {
+variable "AWS_PROFILE" {
   description = "AWS profile to run commands as."
   type = string
 }
 provider "aws" {
-  profile = var.aws_profile
+  profile = var.AWS_PROFILE
   # Bug: region shouldn't be required but is: https://github.com/terraform-providers/terraform-provider-aws/issues/7750
-  region = var.aws_region
+  region = var.AWS_REGION
 }
 
+// Issue around removing tf warnings for undeclared variables: https://github.com/hashicorp/terraform/issues/22004
+variable "AWS_ACCOUNT_ID" { description = "Not used. Is here to stop Terraform warnings." }
+
+variable "cloudflare_account_id" {
+  description = "Used in cloudflare provider."
+  type = string
+}
+variable "cloudflare_api_token" {
+  description = "Used in cloudflare provider."
+  type = string
+}
+provider "cloudflare" {
+  account_id = var.cloudflare_account_id
+  api_token = var.cloudflare_api_token
+}
