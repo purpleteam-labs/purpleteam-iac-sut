29e323744327b0143b6d3d632235b5372e11dd45
Kim Carter <Kim.Carter@binarymist.net>
2020-07-26 17:27:34 +1200

    Removed unneccessary attribute

--- tf/roots/2_nw/variables.tf
+++ tf/roots/2_nw/variables.tf
@@ -39,7 +39,6 @@ variable "suts_attributes" {
   description = "The attributes that apply to each specific SUT."  
   type = map(object({
     // Populate with properties as required
-    id = number
     pt_lb_listener_port = number
   }))
 }
