e65f1093a834da5a1219b235d3f08e6a3da6cd8e
Kim Carter <Kim.Carter@binarymist.net>
2020-07-28 23:52:22 +1200

    Removed depricated Interpolation-only expressions

--- tf/roots/2_nw/nw.tf
+++ tf/roots/2_nw/nw.tf
@@ -24,13 +24,13 @@ locals {
 
 module "vpc" {
   source = "../../modules/common/aws/network/vpc"
-  cidr = "${var.vpc_cidr}"
-  vpc_tags = "${local.vpc_tags}"
+  cidr = var.vpc_cidr
+  vpc_tags = local.vpc_tags
 }
 
 module "subnets" {
   source = "../../modules/common/aws/network/subnets"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id = module.vpc.vpc_id
   subnets_properties = local.subnets_properties
 }
 
@@ -54,18 +54,18 @@ module "nlbPrivateIps" {
 
 module "securityGroups" {
   source = "../../modules/common/aws/network/securityGroups"
-  vpc_id_default = "${module.vpc.vpc_id_default}"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id_default = module.vpc.vpc_id_default
+  vpc_id = module.vpc.vpc_id
   admin_source_ips = var.admin_source_ips
-  non_default_vpc_name = "${local.vpc_tags.Name}"
+  non_default_vpc_name = local.vpc_tags.Name
   aws_nlb_ips = module.nlbPrivateIps.aws_nlb_ips
 }
 
 module "NACLs" {
   source = "../../modules/common/aws/network/aCLs"
-  vpc_id = "${module.vpc.vpc_id}"
-  default_network_acl_id_of_default_vpc = "${module.vpc.default_network_acl_id_of_default_vpc}"
-  default_network_acl_id_of_main_vpc = "${module.vpc.default_network_acl_id_of_main_vpc}"
+  vpc_id = module.vpc.vpc_id
+  default_network_acl_id_of_default_vpc = module.vpc.default_network_acl_id_of_default_vpc
+  default_network_acl_id_of_main_vpc = module.vpc.default_network_acl_id_of_main_vpc
   pt_nACL = var.pt_nACL
   public_and_2nd_mandatory_subnet_for_lb_subnet_ids = local.public_and_2nd_mandatory_subnet_for_lb_subnet_ids
 }
@@ -74,6 +74,6 @@ module "NACLs" {
 // Currently this just creates the Sydney gateway for the Sydney VPC
 module "internetGateways" {
   source = "../../modules/common/aws/network/internetGateways"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id = module.vpc.vpc_id
   igw_tags = local.igw_tags
 }
