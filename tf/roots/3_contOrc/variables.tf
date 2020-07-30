567689b9f3b02edeb481458abec1d4c0e4bac8c5
Kim Carter <Kim.Carter@binarymist.net>
2020-07-30 21:58:34 +1200

    Tweaked & added contOrc variables

--- tf/roots/3_contOrc/variables.tf
+++ tf/roots/3_contOrc/variables.tf
@@ -1,14 +1,25 @@
-variable "aws_region" {
+variable "AWS_REGION" {
   description = "AWS region in use."
   type = string
 }
-variable "aws_profile" {
+variable "AWS_PROFILE" {
   description = "AWS profile to run commands as."
   type = string
 }
 provider "aws" {
-  profile = var.aws_profile
+  profile = var.AWS_PROFILE
   # Bug: region shouldn't be required but is: https://github.com/terraform-providers/terraform-provider-aws/issues/7750
-  region = var.aws_region
+  region = var.AWS_REGION
 }
 
+// Issue around removing tf warnings for undeclared variables: https://github.com/hashicorp/terraform/issues/22004
+variable "AWS_ACCOUNT_ID" { description = "Not used. Is here to stop Terraform warnings." }
+
+variable "cloudflare_account_id" { description = "Not used. Is here to stop Terraform warnings." }
+variable "cloudflare_api_token" { description = "Not used. Is here to stop Terraform warnings." }
+
+// Consume nw outputs.
+variable "aws_lb_target_groups" {
+  description = "Used in creation of ECS Service, and Autoscaling Group."
+  type = string
+}
