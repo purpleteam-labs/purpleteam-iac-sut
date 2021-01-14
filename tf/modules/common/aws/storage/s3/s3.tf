42015d699de9cb1c7dc1174350a1fc51a38d003a
Kim Carter <Kim.Carter@binarymist.net>
2021-01-15 01:00:56 +1300

    Added s3 bucket access logging & public access block at Snyk request

--- tf/modules/common/aws/storage/s3/s3.tf
+++ tf/modules/common/aws/storage/s3/s3.tf
@@ -15,6 +15,11 @@ resource "aws_s3_bucket" "sut_public_keys" {
   acl    = "private"
   // provider?
 
+  logging {
+    target_bucket = aws_s3_bucket.sut_public_keys_log_bucket.id
+    target_prefix = "log/"
+  }
+
   tags = {
     Name        = "SSH public keys for ec2 instances."
     source      = "iac-contOrc-s3"
@@ -27,4 +32,17 @@ resource "aws_s3_bucket_object" "sut_public_keys" {
   key = each.key
   bucket = aws_s3_bucket.sut_public_keys.id
   content = each.value
+}
+
+resource "aws_s3_bucket" "sut_public_keys_log_bucket" {
+  bucket = "sut-public-keys-log-bucket"
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_public_access_block" "sut_public_keys" {
+  bucket = aws_s3_bucket.sut_public_keys.id
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
