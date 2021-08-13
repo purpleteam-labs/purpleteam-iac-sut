82b1a7b975f151d2fb5b754f5b335d69546068c8
Kim Carter <Kim.Carter@binarymist.net>
2021-08-13 18:47:49 +1200

    Tweak copyright notices

--- tf/modules/common/aws/compute/ec2/autoscaling/input.tf
+++ tf/modules/common/aws/compute/ec2/autoscaling/input.tf
@@ -1,11 +1,11 @@
 // Copyright (C) 2017-2021 BinaryMist Limited. All rights reserved.
 
-// This file is part of purpleteam.
+// This file is part of PurpleTeam.
 
-// purpleteam is free software: you can redistribute it and/or modify
+// PurpleTeam is free software: you can redistribute it and/or modify
 // it under the terms of the MIT License.
 
-// purpleteam is distributed in the hope that it will be useful,
+// PurpleTeam is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // MIT License for more details.
